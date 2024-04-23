0071f5185a0cdb1845ff5979e55708d74dcf96eb
KiritoCalgeta <cal14usher@gmail.com>
2024-04-23 15:26:22 +0100

    add first question to JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+    }
+]
