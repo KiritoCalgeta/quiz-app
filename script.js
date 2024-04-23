67a700df518211621b443aaf23a00ec2661840f5
KiritoCalgeta <cal14usher@gmail.com>
2024-04-23 15:30:07 +0100

    Added more questions

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ const questions = [
             {text: "Blue Whale", correct: true},
             {text: "Elephant", correct: false},
             {text: "Giraffe", correct: false},
-
+        ]
+    },
+    {
+        question: "What is my first name",
+        answers: [
+            {text: "Callum", correct: true},
+            {text: "Cal", correct: false},
+            {text: "Pops", correct: false},
+            {text: "William", correct: false},
+        ]
+    },
+    {
+        question: "Where do I live?",
+        answers: [
+            {text: "France", correct: false},
+            {text: "South Korea", correct: false},
+            {text: "London", correct: false},
+            {text: "Edinburgh", correct: true},
+        ]
+    },
+    {
+        question: "What is my favourite anime?",
+        answers: [
+            {text: "Sword Art Online", correct: true},
+            {text: "Dragonball", correct: false},
+            {text: "Seraph of the End", correct: false},
+            {text: "One Punch Man", correct: false},
         ]
     }
 ]
